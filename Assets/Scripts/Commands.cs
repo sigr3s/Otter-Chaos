a6c4236038bf0a509a156721535181a2bbf188d8
Sigr3s <tortosa95@gmail.com>
2020-01-31 19:40:08 +0100

    Basic folders

---
+++ Assets/Scripts/Commands.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Commands", menuName = "Twitch/Commands", order = 0)]
+public class Commands : ScriptableObject {
+    public string join;
+    public List<string> actions;
+    
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public Commands commands;
+
+
+    private void Awake() {
+        instance = this;
+    }
+
+    private void Start() {
+        
+    }
+}
