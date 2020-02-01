aaecb4352c57f75ee2289184334d9e1e55f295e1
Adrià <adria.abella.93@gmail.com>
2020-02-01 20:59:13 +0100

    Added shake and spawnpoints.

---
+++ Assets/Scripts/ShakeBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ShakeBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        transform.DOShakePosition(9999.0f, 0.02f, 1);
+        transform.DOShakeRotation(9999.0f, 0.02f, 1);
+    }
+
+    void Update()
+    {
+        
+    }
+}
