150905a69145c557c5b89b8c2afb2c4ea7bab232
Sigr3s <tortosa95@gmail.com>
2020-02-01 18:53:05 +0100

    Twitch channel

--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -7,19 +7,27 @@ using UnityEngine.UI;
 public class TitleScreenManager : MonoBehaviour
 {
     [SerializeField] private Button startGame = null;
+    [SerializeField] private InputField twitchChannel;
 
     void Start()
     {
         startGame.onClick.AddListener(OnStartButtonPressed);
+
+        if(PlayerPrefs.HasKey("TwitchChannel")){
+            twitchChannel.text = PlayerPrefs.GetString("TwitchChannel");
+        }
     }
 
     void Update()
     {
-        
+        startGame.interactable =  !string.IsNullOrEmpty(twitchChannel.text);
     }
 
     public void OnStartButtonPressed()
     {
+        PlayerPrefs.SetString("TwitchChannel", twitchChannel.text);
+        PlayerPrefs.Save();
+    
         SoundManager.instance.PlayGameStart();
         Invoke("LoadSceneGame", 2.2f);
     }
